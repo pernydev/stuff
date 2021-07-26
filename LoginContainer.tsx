9beea51fe3234a680a7251a423e5b7ccc99bcaff
pernydev <83672513+pernydev@users.noreply.github.com>
2021-07-26 16:26:50 +0300

    Update LoginContainer.tsx

--- LoginContainer.tsx
+++ LoginContainer.tsx
@@ -118,7 +118,7 @@ const LoginContainer = ({ history }: RouteComponentProps) => {
                         >
                             Password reset
                         </Link>
-                        <p> </p>
+                        <p>⠀</p>
                             <a css={tw`text-xs text-neutral-500 tracking-wide no-underline uppercase hover:text-neutral-600`} href="/auth/register">Register</a>
                     </div>
                 </LoginFormContainer>
